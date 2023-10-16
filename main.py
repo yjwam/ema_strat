832c01d452aed945b1d158ddf5e8320c62254bb5
yjwam <yashjainurffvikky@gmail.com>
2023-10-16 23:12:22 +0530

    debugged

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ from ib_insync import *
 import json
 import os
 import schedule
-import time
+import pytz
 
 ib = IB()
 ib.connect('127.0.0.1', 7497, clientId=1)
+
+est = pytz.timezone('US/Eastern')
+
 
 def create_ib_contract(contract,ib):
     contract = Contract(symbol=contract['symbol'], secType=contract['secType'], exchange=contract['exchange'], currency=contract['currency'], includeExpired=True)
@@ -31,6 +34,7 @@ def get_historical_data(contract,barsize,debugging = False):
             data = util.df(bars)['close']
         except:
             ib.sleep(1)
+            print("Retrying to get historical data")
             data = get_historical_data(contract,barsize,debugging = False)
     return data
 
@@ -75,27 +79,35 @@ def update_results(path,contract,trade,first = False):
     return None
 
 def trade_time(contract_info,ib,debugging=False):
-    print("Start Algo")
+    print("Starting Algorithm")
     path = "orders"
     contract = create_ib_contract(contract_info['contract'],ib)
+    # check any open positions
+    print("Checking Open Position")
+    open_pos,open_pos_dict = check_open_orders(path,contract)
+    if not open_pos:
+        print("No Open Position Found")
+
     trailing_stop = contract_info['trailing_stop_pct']
     taking_profit = contract_info["take_profit_pct"]
     quantity = contract_info['no_contract']
-    hist_data = get_historical_data(contract,contract_info['ema_interval'],debugging)  #yahoo finacnce need to repalce with ib one
-    emas = contract_info['emas']
-    low_ema = ema(hist_data,emas[0])
-    high_ema = ema(hist_data,emas[1])
-    print("low ema :",low_ema)
-    print("high ema :",high_ema)
-    # check any open positions
-    open_pos,open_pos_dict = check_open_orders(path,contract)
     if not open_pos:
+
+        hist_data = get_historical_data(contract,contract_info['ema_interval'],debugging)
+        print("Calculating EMAs")
+        emas = contract_info['emas']
+        low_ema = ema(hist_data,emas[0])
+        high_ema = ema(hist_data,emas[1])
+        print(" low ema :",low_ema)
+        print(" high ema :",high_ema)
+
         if low_ema > high_ema and contract_info['action'] == "BUY":
             position = 1
             trade = place_order(contract,ib,"BUY",quantity)
             traded_price = trade.fills[0].execution.price
             stop_loss = (1-trailing_stop/100)*traded_price
             update_results(path,contract,trade,True)
+            print("Taking Long Position")
 
         elif low_ema > high_ema and contract_info['action'] == "SELL":
             position = -1
@@ -103,50 +115,49 @@ def trade_time(contract_info,ib,debugging=False):
             traded_price = trade.fills[0].execution.price
             stop_loss = (1+trailing_stop/100)*traded_price
             update_results(path,contract,trade,True)
+            print("Taking Short Position")
 
         else:
             position = 0
     else:
+        print("Open Position Found")
         traded_price = open_pos_dict['entry_price']
         position = 1 if open_pos_dict["long/short"] == "long" else -1
         stop_loss = (1-trailing_stop/100)*traded_price if position == 1 else (1+trailing_stop/100)*traded_price
 
-
-    current_price = live_data(contract,ib,debugging)
-    print(datetime.datetime.now()," Current Price :",current_price)
     while position != 0:
         current_price = live_data(contract,ib,debugging)
         print(datetime.datetime.now()," Current Price :",current_price)
         if position*(current_price/traded_price-1)*100 >= taking_profit:
             if position > 0:
                 position = 0
-                print("taking profit")
+                print("Taking Profit")
                 trade = place_order(contract,ib,"SELL",quantity)
                 update_results(path,contract,trade)
                 continue
             else:
                 position = 0
-                print("taking profit")
+                print("Taking Profit")
                 trade = place_order(contract,ib,"BUY",quantity)
                 update_results(path,contract,trade)
                 continue
         if (current_price - stop_loss)*position < 0:
             if position > 0:
                 position = 0
-                print("stop loss hit")
+                print("Stop Loss Hit")
                 trade = place_order(contract,ib,"SELL",quantity)
                 update_results(path,contract,trade)
                 continue
             else:
                 position = 0
-                print("stop loss hit")
+                print("Stop Loss Hit")
                 trade = place_order(contract,ib,"BUY",quantity)
                 update_results(path,contract,trade)
                 continue
         new_stop = (1-position*contract_info['trailing_stop_pct']/100)*current_price
         if  (new_stop - stop_loss)*position >= 0:
             stop_loss = new_stop
-            print('trailing stop',stop_loss)
+            print('Trailing Stop Loss',stop_loss)
     return False
 
 def check_open_orders(path,contract):
@@ -167,14 +178,17 @@ def main():
         contract_info = json.load(f)
     schedule.every().minute.at(":00").do(trade_time, contract_info = contract_info, ib = ib, debugging = debugging)
     while True:
+        now = datetime.datetime.now()
+        est_time = now.astimezone(est).time()
+        if est_time > datetime.datetime(2023,1,1,16,0).time():
+            print("Market Closed")
+            break
         try:
             schedule.run_pending()
         except Exception as e:
             if debugging:
                 raise e
             continue
-        # if datetime.datetime.now().time() > datetime.datetime(2023,1,1,16,0).time():
-        #     break
     return None
 
 if __name__ == "__main__":
